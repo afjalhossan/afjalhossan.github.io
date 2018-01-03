b12395599a2545e549c7bbe983e6794d0f4d5c37
afjalhossan <its4afjal@gmail.com>
2018-01-03 12:55:21 +0600

    79

---
+++ assignment/79/js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+    "use strict";
+    jQuery(document).ready(function($){
+	   $('.mainchimp-form').ajaxChimp({
+           url:'http://github.us17.list-manage.com/subscribe/post?u=cab2061b52455974538e199a5&amp;id=421c37eedc'
+       });
+
+    jQuery(window).load(function($){
+        
+        
+        
+    });
+
+}(jQuery));
+	
+	
+	
+
+	
+	

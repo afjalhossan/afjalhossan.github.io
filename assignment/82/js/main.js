1d87a272f2b4de45bdcc4f543b56bafd1efae285
afjalhossan <its4afjal@gmail.com>
2018-01-06 14:39:23 +0600

    82

---
+++ assignment/82/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+    "use strict";
+    jQuery(document).ready(function($){
+	
+		$(".featured-projects").owlCarousel({
+           items:1,
+            nav:false,
+            dots:true
+        });
+    });
+    
+    jQuery(window).load(function($){
+        
+        
+        
+    });
+
+}(jQuery));
+	
+	
+	
+
+	
+	

9a90d9f68dd95968a73edf4e056e0badf7c682e2
afjalhossan <its4afjal@gmail.com>
2018-01-07 12:46:14 +0600

    85

---
+++ assignment/85/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+    "use strict";
+    jQuery(document).ready(function($){
+	$(".testimonial-carousel").owlCarousel({
+		items:1,
+		loop:true,
+		nav:true,
+		dots:false,
+		navText:['<i class="fa fa-angle-left">','<i class="fa fa-angle-right">'],
+		autoPlay:false
+	});
+		
+    });
+    
+    jQuery(window).load(function($){
+        
+        
+        
+    });
+
+}(jQuery));
+	
+	
+	
+
+	
+	

8f6c99c439701f19ce38f1f965619762ed4fbda4
afjalhossan <its4afjal@gmail.com>
2018-01-08 11:56:27 +0600

    87

---
+++ assignment/87/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+    "use strict";
+    jQuery(document).ready(function($){
+	
+		$('.homepage-slider').owlCarousel({
+			items:1,
+			dots:true,
+			nav:true,
+			loop:true,
+			autoPlay:false,
+			navText:['<i class="fa fa-arrow-left"></i>','<i class="fa fa-arrow-right"></i>'],
+			dotsData:true,
+		});
+    });
+    
+    jQuery(window).load(function($){
+        
+        
+        
+    });
+
+}(jQuery));
+	
+	
+	
+
+	
+	

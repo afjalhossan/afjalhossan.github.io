15c53ce6e01335382d66ca9c40ceca4e479cfce8
afjalhossan <its4afjal@gmail.com>
2018-01-10 10:57:47 +0600

    91

---
+++ assignment/91/js/main.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($){
+    $(".slider-area").owlCarousel({
+        items:1,
+        dots:true,
+        loop:true,
+        nav:false,
+        autoPlay:false
+    });
+});

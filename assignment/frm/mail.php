2a0d12404b6351f5c86be2c63bec100a605e6ba1
afjalhossan <its4afjal@gmail.com>
2018-02-24 15:33:42 +0600

    ffdfdf

--- assignment/frm/mail.php
+++ assignment/frm/mail.php
@@ -6,12 +6,11 @@
         $name = strip_tags(trim($_POST["name"]));
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $lname = trim($_POST["lname"]);
-        $subject = trim($_POST["subject"]);
+        $number = trim($_POST["number"]);
         $message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($subject) OR empty($lname) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($name) OR  empty($number) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Please complete the form and try again.";
@@ -20,16 +19,15 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "mdsalim400@gmail.com";
+        $recipient = "its4afjal@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
 
         // Build the email content.
-        $email_content = "First Name: $name\n";
-        $email_content = "Last Name: $lname\n";
+        $email_content = " Name: $name\n";
+        $email_content = "Number: $lname\n";
         $email_content .= "Email: $email\n\n";
-        $email_content .= "Subject: $subject\n\n";
         $email_content .= "Message:\n$message\n";
 
         // Build the email headers.
